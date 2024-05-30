c521cf2076c272f1d443a3e949d695d52e3a5f00
s.gudivada@techmitts.com <s.gudivada@techmitts.com>
2024-05-30 13:21:11 +0530

    Intial template

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let currentIndex = 0;
+const images = document.querySelectorAll('.carousel-container img');
+const totalImages = images.length;
+
+function showNextImage() {
+    const container = document.querySelector('.carousel-container');
+    currentIndex = (currentIndex + 1) % totalImages;
+    container.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+setInterval(showNextImage, 3000); // Change image every 3 seconds
